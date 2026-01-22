f0a11dbd2683434c145d66d359c01a3217343df8
AslamAsh <93033181+AslamAsh@users.noreply.github.com>
2026-01-22 21:29:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let url = window.location.href;
+
+// Count number of letters in the URL
+let length = url.length;
+
+// Show result using alert
+alert("The length of the URL is: " + length);
